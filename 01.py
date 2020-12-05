63e15e545aa97c8f63513c8164a5cb03662d94a9
Josef Bukovský <bukovskyjosef@gmail.com>
2020-12-05 11:02:51 +0100

    Day 1 - second star

--- 01.py
+++ 01.py
@@ -1,10 +1,25 @@
 # First star 
 file1 = open('01.txt', 'r')
-Lines = file1.readlines()
+lines = file1.read().splitlines()
 
-for n, line in enumerate(Lines): 
-  for i in range(len(Lines)-n):
-    if (int(Lines[n+i]))+int(line) == 2020:
-      hello = int(line) * int(Lines[n+i])
+for n, line in enumerate(lines):
+  for i in range(len(lines)-n):
+    if (int(lines[n+i]))+int(line) == 2020:
+      hello = int(line) * int(lines[n+i])
 
-print (hello)
+print(hello)
+
+# Second star
+doubles = []
+
+for n, line in enumerate(lines):
+  for i in range(len(lines)-n):
+    if ((int(lines[n+i]))+int(line)) < 2020:
+        doubles.append([(int(lines[n+i])), int(line), (int(lines[n+i]))+int(line)])
+
+for single in lines:
+    count = 0
+    for i in doubles:
+        if (doubles[count][2]+int(single) == 2020):
+            print(doubles[count][0]*doubles[count][1]*int(single))
+        count = count + 1
