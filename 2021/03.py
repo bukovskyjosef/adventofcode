c2dee432c9e149b1dcf207acbbdd80a8cb2d4c48
Josef Bukovský <bukovskyjosef@gmail.com>
2021-12-13 10:05:04 +0100

    Teda, u pouziti metod jsem se hodne zapotil a podminka ze musim prestat u jednoho prvku je dost osemetna.

--- 2021/03.py
+++ 2021/03.py
@@ -1,53 +1,82 @@
 file = open('03input.txt', 'r')
 lines = file.read().splitlines()
 
-numberOfColumns = int(len(lines[0]))
-report = int(len(lines))
-
-print("Délka řádku je " + str(numberOfColumns) + " prvků.\nDiagnostický report má " + str(report) + " řádků.")
-
-#naplneni registru poly polí
-registr = []
-
-for i in range(numberOfColumns):
-    temp = []
-    for j in range(report):
-        temp.append(lines[j][i])
-    registr.append(temp)
-
-#výpočet četnosti prvků
-i = 0
-j = 0
-temp = []
-
-for i in range(int(len(registr))):
-    temp = registr[i]
-    #print(registr[i])
-    nula = sum(1 for item in temp if item==("0"))
-    jednicka = sum(1 for item in temp if item==("1"))
-    #print(nula)
-    #print(jednicka)
-
-    if(nula > jednicka):
-        registr[i] = 0
+#metoda vratVyssiCetnost() vrátí větší z hodnot 0,1 na zadané pozici ve stringu
+#@pole seznam hodnot k proházení
+#@pozice kterou pozici počítáme
+def vratVyssiCetnost(pole, pozice):
+    nula = 0
+    jednicka = 0
+    for i in range(len(pole)):
+        if (pole[i][pozice] == "1"):
+            jednicka += 1
+        elif (pole[i][pozice]=="0"):
+            nula += 1
+        else:
+            print("error, zadaná hodnota není ani jednička ani nula")
+    if nula > jednicka:
+        zjistenaHodnota = 0
+    elif nula == jednicka:
+        zjistenaHodnota = 1
     else:
-        registr[i] = 1
+        zjistenaHodnota = 1
+    return zjistenaHodnota
 
-print("Výsledek registru:" + str(registr))
-
-binarniCislo0 = ""
-binarniCislo1 = ""
-
-for o in registr:
-    if (registr[o] ==0):
-        binarniCislo0 = binarniCislo0 + "0"
-        binarniCislo1 = binarniCislo1 + "1"
+# metoda vratNizsiCetnost() vrátí menší z hodnot 0,1 na zadané pozici ve stringu
+# @pole seznam hodnot k proházení
+# @pozice kterou pozici počítáme
+def vratNizsiCetnost(pole, pozice):
+    nula = 0
+    jednicka = 0
+    for i in range(len(pole)):
+        if (pole[i][pozice] == "1"):
+            jednicka += 1
+        elif (pole[i][pozice]=="0"):
+            nula += 1
+        else:
+            print("error, zadaná hodnota není ani jednička ani nula")
+    if nula < jednicka:
+        zjistenaHodnota = 0
+    elif nula == jednicka:
+        zjistenaHodnota = 0
     else:
-        binarniCislo0 = binarniCislo0 + "1"
-        binarniCislo1 = binarniCislo1 + "0"
+        zjistenaHodnota = 1
+    return zjistenaHodnota
 
-print("binar0 " + str(binarniCislo0))
-print("binar1 " + str(binarniCislo1))
+# metoda filtruj() vytvori nove pole, ktere obsahuje pouze polozky ze vstupniho pole,
+# ktere maji na n-te pozici hodnotu definovanou podminkou
+# @lines vstupni pole
+# @podminka hodnota pro porovnani
+# @pozice porovnavana pozice
+def filtruj(lines, podminka, pozice):
+    filtrovanePole=[]
+    for i in range(len(lines)):
+        if (lines[i][pozice] == str(podminka)):
+            filtrovanePole.append(lines[i])
+    return filtrovanePole
+
+pozice = 0
+for i in range(len(lines[0])):
+    if len(lines)>1:
+        lines = filtruj(lines,vratVyssiCetnost(lines,pozice),i)
+    pozice += 1
+
+binarniCislo0 = str(lines[0])
+print("binarniCislo0 = " + str(binarniCislo0) + " = " + str(int(binarniCislo0, 2)))
+
+pozice = 0
+file = open('03input.txt', 'r')
+lines = file.read().splitlines()
+
+for j in range(len(lines[0])):
+    if len(lines)>1:
+        lines = filtruj(lines,vratNizsiCetnost(lines,pozice),j)
+    pozice += 1
+
+binarniCislo1 = str(lines[0])
+print("binarniCislo1 = " + str(binarniCislo1) + " = " + str(int(binarniCislo1, 2)))
 
 vysledek = int(binarniCislo0, 2) * int(binarniCislo1, 2)
-print(vysledek)
+print("Výsledek: " + str(int(binarniCislo0, 2)) + " * " + str(int(binarniCislo1, 2)) + " = " + str(vysledek))
+
+#3654234 too high
