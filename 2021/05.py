07bcabd13cc913df5aa11dc5d85d6f29206c984a
Josef Bukovský <bukovskyjosef@gmail.com>
2021-12-17 01:45:45 +0100

    funkcni male more

--- 2021/05.py
+++ 2021/05.py
@@ -2,7 +2,7 @@ import re
 
 file = open('05input.txt', 'r')
 lines = file.read().splitlines()
-lines = ["0,1 -> 0,4"]
+lines = ["2,3 -> 2,4"]
 
 # metoda vezme ze zadani pouza ta pole kde x1=x2 nebo y1=y2
 def sliceIt(_lines):
@@ -24,11 +24,20 @@ def sliceIt(_lines):
 
 
 def draw_lines(_filtered_lines, _sea):
-    print(_filtered_lines)
 
-    for radek in range(_filtered_lines[0][0],_filtered_lines[0][2]):
-        print("a")
+    x1 = _filtered_lines[0][0] # 1
+    x2 = _filtered_lines[0][2] # 1
+    y1 = _filtered_lines[0][1] # 1
+    y2 = _filtered_lines[0][3] # 4
 
+    for sloupec in range(len(_sea)):
+        for radek in range(len(_sea)):
+            if radek >= x1 and radek <= x2 and sloupec >= y1 and sloupec <= y2:
+                sea[sloupec][radek] =+ 1
+
+    print("\nnove more")
+    for i in range(len(sea)):
+        print(sea[i])
     return
 
 
@@ -46,7 +55,7 @@ def create_sea(size):
 
 sea = create_sea(5)
 
-print()
+print("\nprazdne more")
 for i in range(len(sea)):
     print(sea[i])
 print()
