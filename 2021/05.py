10d7f541877ad6f40f637adf64896d142c8c1872
Josef Bukovský <bukovskyjosef@gmail.com>
2021-12-17 02:25:40 +0100

    finshed day 5 first part

--- 2021/05.py
+++ 2021/05.py
@@ -2,7 +2,7 @@ import re
 
 file = open('05input.txt', 'r')
 lines = file.read().splitlines()
-lines = ["2,3 -> 2,4"]
+#lines = ["0,0 -> 0,4", "0,1 -> 3,1","0,0 -> 0,3"]
 
 # metoda vezme ze zadani pouza ta pole kde x1=x2 nebo y1=y2
 def sliceIt(_lines):
@@ -11,12 +11,32 @@ def sliceIt(_lines):
 
     for i in range(len(lines)):
         temp = re.split(' |,|-|>', _lines[i])
-
+        #print(lines[i])
         if temp[0] == temp[5] or temp[1] == temp[6]:
-            temp2.append(int(temp[0]))
-            temp2.append(int(temp[1]))
-            temp2.append(int(temp[5]))
-            temp2.append(int(temp[6]))
+
+            x1_temp = int(temp[0])
+            x2_temp = int(temp[5])
+            y1_temp = int(temp[1])
+            y2_temp = int(temp[6])
+
+            if x1_temp > x2_temp:
+                x2 = x1_temp
+                x1 = x2_temp
+            else:
+                x1 = x1_temp
+                x2 = x2_temp
+
+            if y1_temp > y2_temp:
+                y2 = y1_temp
+                y1 = y2_temp
+            else:
+                y1 = y1_temp
+                y2 = y2_temp
+
+            temp2.append(x1) #x1
+            temp2.append(y1) #y1
+            temp2.append(x2) #x2
+            temp2.append(y2) #y2
             final_list.append(temp2)
         temp2 = []
 
@@ -25,20 +45,21 @@ def sliceIt(_lines):
 
 def draw_lines(_filtered_lines, _sea):
 
-    x1 = _filtered_lines[0][0] # 1
-    x2 = _filtered_lines[0][2] # 1
-    y1 = _filtered_lines[0][1] # 1
-    y2 = _filtered_lines[0][3] # 4
+    for line in range(len(_filtered_lines)):
+        x1 = _filtered_lines[line][0] # 1
+        x2 = _filtered_lines[line][2] # 1
+        y1 = _filtered_lines[line][1] # 1
+        y2 = _filtered_lines[line][3] # 4
 
-    for sloupec in range(len(_sea)):
-        for radek in range(len(_sea)):
-            if radek >= x1 and radek <= x2 and sloupec >= y1 and sloupec <= y2:
-                sea[sloupec][radek] =+ 1
+        for sloupec in range(len(_sea)):
+            for radek in range(len(_sea)):
+                if radek >= x1 and radek <= x2 and sloupec >= y1 and sloupec <= y2:
+                    sea[sloupec][radek] += 1
 
-    print("\nnove more")
-    for i in range(len(sea)):
-        print(sea[i])
-    return
+    # print("\nnove more")
+    # for i in range(len(sea)):
+    #     print(sea[i])
+    # print()
 
 
 def create_sea(size):
@@ -53,14 +74,27 @@ def create_sea(size):
     return temp_sea
 
 
-sea = create_sea(5)
+sea = create_sea(999)
 
-print("\nprazdne more")
-for i in range(len(sea)):
-    print(sea[i])
-print()
 
 # priprav zadani ve forme poli
 filtered_lines = sliceIt(lines)
 
-draw_lines(filtered_lines,sea)
+updated_sea = draw_lines(filtered_lines,sea)
+
+pocet = 0
+
+for line in range(len(sea)):
+    for item in range(len(sea[line])):
+        if sea[line][item] >= 2:
+            pocet += 1
+
+print(pocet)
+
+# too high 873923
+# too high 47848
+# too low 1297
+# correct 4826
+
+# 2nd part
+# 954973 too high
