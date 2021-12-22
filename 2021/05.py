c83f15d042cccb9e774b7b727dcb44ca57ae9613
Josef Bukovský <bukovskyjosef@gmail.com>
2021-12-22 13:56:49 +0100

    finished day 5, 2nd part. That was hard.

--- 2021/05.py
+++ 2021/05.py
@@ -2,64 +2,121 @@ import re
 
 file = open('05input.txt', 'r')
 lines = file.read().splitlines()
-#lines = ["0,0 -> 0,4", "0,1 -> 3,1","0,0 -> 0,3"]
+# lines = ["1,1 -> 1,4"]
 
-# metoda vezme ze zadani pouza ta pole kde x1=x2 nebo y1=y2
+
 def sliceIt(_lines):
     temp2 = []
-    final_list = []
+    final_list_unordered = []
 
     for i in range(len(lines)):
         temp = re.split(' |,|-|>', _lines[i])
-        #print(lines[i])
-        if temp[0] == temp[5] or temp[1] == temp[6]:
-
-            x1_temp = int(temp[0])
-            x2_temp = int(temp[5])
-            y1_temp = int(temp[1])
-            y2_temp = int(temp[6])
-
-            if x1_temp > x2_temp:
-                x2 = x1_temp
-                x1 = x2_temp
-            else:
-                x1 = x1_temp
-                x2 = x2_temp
-
-            if y1_temp > y2_temp:
-                y2 = y1_temp
-                y1 = y2_temp
-            else:
-                y1 = y1_temp
-                y2 = y2_temp
-
-            temp2.append(x1) #x1
-            temp2.append(y1) #y1
-            temp2.append(x2) #x2
-            temp2.append(y2) #y2
-            final_list.append(temp2)
+        temp2.append(int(temp[0]))
+        temp2.append(int(temp[1]))
+        temp2.append(int(temp[5]))
+        temp2.append(int(temp[6]))
+        final_list_unordered.append(temp2)
         temp2 = []
 
-    return final_list
+    return final_list_unordered
 
 
-def draw_lines(_filtered_lines, _sea):
+def draw_lines(_lines, _sea):
+    for line in range(len(_lines)):
+        x1 = _lines[line][0]
+        x2 = _lines[line][2]
+        y1 = _lines[line][1]
+        y2 = _lines[line][3]
 
-    for line in range(len(_filtered_lines)):
-        x1 = _filtered_lines[line][0] # 1
-        x2 = _filtered_lines[line][2] # 1
-        y1 = _filtered_lines[line][1] # 1
-        y2 = _filtered_lines[line][3] # 4
+        x1_temp = _lines[line][0]
+        x2_temp = _lines[line][2]
+        y1_temp = _lines[line][1]
+        y2_temp = _lines[line][3]
 
-        for sloupec in range(len(_sea)):
-            for radek in range(len(_sea)):
-                if radek >= x1 and radek <= x2 and sloupec >= y1 and sloupec <= y2:
-                    sea[sloupec][radek] += 1
+        # horizontalni tam
+        # 0,0 -> 0,4  x1 == x2 and y1 < y2
+        # horizontalni  zpět
+        # 0,4 -> 0,0  x1 == x2 and y1 > y2
+        #
+        # vertikální tam
+        # 0,0 -> 2,0  x1 < x2 and y1 == y2
+        # vertikální zpět
+        # 2,0 -> 0,0  x1 > x2 and y1 == y2
+        #
+        # úhlopříčka tam
+        # 2,0 -> 4,2  x2-x1 == y2-y1 and x1 < x2 and y1 < y2
+        # úhlopříčka zpět
+        # 4,2 -> 2,0  x1-x2 == y1-y2 and x1 > x2 and y1 > y2
+        #
+        # uhlopříčka tam 2
+        # 0,4 -> 3,1  x2-x1 == y1-y2 and x1 < x2 and y1 > y2
+        # úhlopříčka zpět 2
+        # 3,1 -> 0,4  x1-x2 == y2-y1 and x1 > x2 and y1 < y2
 
-    # print("\nnove more")
-    # for i in range(len(sea)):
-    #     print(sea[i])
-    # print()
+        # menší dopředu pro horizontální
+        if x1 == x2 and y1 > y2:
+            y1 = y2_temp
+            y2 = y1_temp
+
+        # menší dopředu pro vertikální
+        if x1 > x2 and y1 == y2:
+            x1 = x2_temp
+            x2 = x1_temp
+
+        # srovnání uhlopříčka zpět a zpět2
+        if (x1 - x2 == y1 - y2 and x1 > x2 and y1 > y2) or (x1 - x2 == y2 - y1 and x1 > x2 and y1 < y2):
+            x1 = x2_temp
+            x2 = x1_temp
+            y1 = y2_temp
+            y2 = y1_temp
+
+        if x1 == x2 and y1 < y2:
+            # print("x1 == x2 and y1 < y2")
+            temp_line = []
+            pocet = y2 - y1
+
+            for i in range(0, pocet+1):
+                temp_line.append([x1, y1 + i])
+
+            for j in range(len(temp_line)):
+                sea[temp_line[j][0]][temp_line[j][1]] += 1
+
+        if x1 < x2 and y1 == y2:
+            # print("x1 < x2 and y1 == y2")
+            temp_line = []
+            pocet = x2 - x1
+
+            for i in range(0, pocet+1):
+                temp_line.append([x1 + i, y1])
+
+            for j in range(len(temp_line)):
+                sea[temp_line[j][0]][temp_line[j][1]] += 1
+
+        if x2 - x1 == y2 - y1 and x1 < x2 and y1 < y2:
+            # print("x2 - x1 == y2 - y1 and x1 < x2 and y1 < y2")
+            temp_line = []
+            pocet = x2 - x1
+
+            for i in range(0, pocet + 1):
+                temp_line.append([x1 + i, y1 + i])
+
+            for j in range(len(temp_line)):
+                sea[temp_line[j][0]][temp_line[j][1]] += 1
+
+        if x2 - x1 == y1 - y2 and x1 < x2 and y1 > y2:
+            # print("x2 - x1 == y1 - y2 and x1 < x2 and y1 > y2")
+            temp_line = []
+            pocet = x2 - x1
+
+            for i in range(0, pocet + 1):
+                temp_line.append([x1 + i, y1 - i])
+
+            for j in range(len(temp_line)):
+                sea[temp_line[j][0]][temp_line[j][1]] += 1
+
+        # for i in range(len(sea)):
+        #     print(sea[i])
+        # print("\n")
 
 
 def create_sea(size):
@@ -75,13 +132,8 @@ def create_sea(size):
 
 
 sea = create_sea(999)
-
-
-# priprav zadani ve forme poli
-filtered_lines = sliceIt(lines)
-
-updated_sea = draw_lines(filtered_lines,sea)
-
+lines = sliceIt(lines)
+updated_sea = draw_lines(lines, sea)
 pocet = 0
 
 for line in range(len(sea)):
@@ -98,3 +150,8 @@ print(pocet)
 
 # 2nd part
 # 954973 too high
+# 20047 too high
+# 16793 CORRECT
+# 16891 wrong
+# 16749 wrong
+# 13974 low
